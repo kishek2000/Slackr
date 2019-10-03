b923a62288646b92e130297373f7b32eebe3578e
Liam <z5254570@ad.unsw.edu.au>
2019-10-03 11:01:34 +1000

    Added Helper Functions

---
+++ server/Iteration_One/Tests/helper_test_functions.py
@@ -0,0 +1,11 @@
+def get_data():
+    return {'uid': 42, 'token': "123456"}
+    
+def get_data_two():
+    return {'uid': 17, 'token': "987654"}
+    
+def valid_uid():
+    pass
+    
+def valid_token:
+    pass
