2ea48a161409ef4cbf47c69bd782018b8fcdfd3c
Shoan Desai <shoan.desai@student.unsw.edu.au>
2019-10-06 13:38:28 +1100

    Update standup_tests.py

--- server/Iteration_One/Tests/standup_tests.py
+++ server/Iteration_One/Tests/standup_tests.py
@@ -1,55 +1,45 @@
+#################################################################################
+##                              STANDUP TESTS                                  ##
+#################################################################################
+
 import pytest
 from datetime import datetime
 
-'''
-Assume an accepted password has the following requirements (A function called 
-is_valid_password() will check against these requirements and return either True or False) :-
-    
-    - Passoword length between 5 and 20 characters inclusive
-    - At least 1 upper character
-    - At least 1 lower case character
-    - At least 1 number
-    - At least 1 special character
-    
-'''
-
 @pytest.fixture
 def registered_user_1():
-    return auth_register("PokemonMaster@gmail.com", "validp@ssword", "Ash", "Ketcham")
+    return auth_register("PokemonMaster@gmail.com", "validp@ssword1", "Ash", "Ketcham")
 
 @pytest.fixture
 def registered_user_2():
-    return auth_register("FreddieMercury.com", "ValidP@sswordQueen", "Freddie", "Mercury")
+    return auth_register("FreddieMercury@gmail.com", "ValidP@sswordQueen1", "Freddie", "Mercury")
     
 @pytest.fixture
 def registered_user_3():
-    return auth_register("TheRealPokemonMaster@gmail.com", "ValidP@sswordRocks", "Gary", "Oak")
-   
+    return auth_register("TheRealPokemonMaster@gmail.com", "ValidP@sswordRocks1", "Gary", "Oak")
     
-@pytest.fixture
-def channel_id_user_1(registered_user_1):
+@pytest.fixture #channel created by registered_user_1
+def channel_id_user_1(registered_user_1): 
     return channels_create(registered_user_1['token'], "ValidChannelName", True)    
-    
-       
-######TESTS FOR standup_start#######
-
+           
+#################################################################################
+##                           TESTING standup_start                             ##
+#################################################################################
 
 def test_standup_start_correct_details(channel_id_user_1, registered_user_1):
 
     user_1_token = registered_user_1['token']
   
-    channel_id = 1
+    channel_id = channel_id_user_1
     
     #Should produce no errors
     standup_start(user_1_token, channel_id_user_1)
+ 
 
-    
-    
 def test_standup_start_nonexistant_channel(registered_user_1):
     
     user_token = registered_user_1['token']
 
-    channel_id = "Nonexistant channnel"
+    channel_id = -1
     
     #Should produce ValueError as channel with channel_id = -1 does not exist
     with pytest.raises(ValueError):
@@ -67,7 +57,7 @@ def test_standup_start_unauthorised_user_access(channel_id_user_1, registered_us
         standup_start(user_token, channel_id)
         
      
-def test_standup_start_time_finish_greater_than_time_current(channel_id_user_1, registered_user_1):
+def test_standup_start_time_finish_greaterThan_time_current(channel_id_user_1, registered_user_1):
     
     time_current = datetime.now()
 
@@ -89,28 +79,29 @@ def test_standup_start_correct_time_difference(channel_id_user_1, registered_use
         
     time_finish = standup_start(user_token, channel_id)
     
+    time_difference = time_finish - time_current 
+    
     #Testing if time_finish has a 15 minute differnce in time from time_current 
-    assert(((time_finish.minute % 60) - (time_current.minute % 60)) == 15 or 
-           ((time_finish.minute % 60) - (time_current.minute % 60)) == -15 )
+    assert((time_difference.seconds/60) == 15)
 
+#################################################################################
+##                           TESTING standup_send                              ##
+#################################################################################
 
-######TESTS FOR standup_send#######   
-
-
-def test_standup_send_valid_inputs__and_multiple_users(channel_id_user_1, registered_user_1, registered_user_2):
-    
-    #Start the standup
-    
+def test_standup_send_valid(channel_id_user_1, registered_user_1, registered_user_2):
+      
     user_token_1 = registered_user_1['token']
-    user_token_2 = registered_user_2['token']
-    
     channel_id = channel_id_user_1     
-     
+    
+    #add registered_user_2 to the channel
+    user_token_2 = registered_user_2['token']
     channel_join(user_token_2, channel_id)   
     
-    time_finish = standup_start(user_1_token, channel_id)
+    #registered_user_1 starts the standup
     
-    #Should produce no error
+    standup_start(user_1_token, channel_id)
+    
+    #Should produce no error as a valid message is sent by both registered_users during the standup
     
     message = "This is a valid message"
           
@@ -119,16 +110,15 @@ def test_standup_send_valid_inputs__and_multiple_users(channel_id_user_1, regist
      
 def test_standup_send_nonexistant_channel(channel_id_user_1, registered_user_1):
     
-    #Start the standup
+    #Start the standup on the channel with channel id channel_id_user_1  
     
     user_token = registered_user_1['token']
     channel_id_1 = channel_id_user_1      
-    time_finish = standup_start(user_token, channel_id_1)
-
+    standup_start(user_token, channel_id_1)
     
     #Should produce ValueError as channel with channel_id = -1 does not exist
     
-    channel_id_2 = "Nonexistant channel"
+    channel_id_2 = -1
     
     message = "This is a valid message"
     
@@ -163,11 +153,11 @@ def test_standup_send_unauthorised_user(channel_id_user_1, registered_user_1, re
     
     #Should produce AccessError as the user is not a member of the channel
     
-    user_token_2 = registered_user_3['token']
+    user_token_3 = registered_user_3['token']
     message = "This is a valid message"
     
     with pytest.raises(AccessError):
-        standup_send(user_token_2, channel_id, message)      
+        standup_send(user_token_3, channel_id, message)      
         
         
         
@@ -177,24 +167,16 @@ def test_standup_send_standup_time_finished(channel_id_user_1, registered_user_1
     
     user_token = registered_user_1['token']
     channel_id = channel_id_user_1     
-    time_finish = standup_start(user_token, channel_id)
-    
-
+    standup_start(user_token, channel_id)
     
     #Sleep 15 minutes
-    sleep(900)
-           
-    time_current = datetime.now()       
     
+    sleep(900)  
     
-    #Standup time should be finished so now sending a message to the standup should produce an AccessError even if the user belongs to the channel
+    #Standup time should be finished so now sending a message to the standup should produce
+    #an AccessError even if the user sending a message to the standup belongs to the channel
     
     message = "This is a valid message"
     
     with pytest.raises(AccessError):
         standup_send(user_token, channel_id, message)   
-        
-        
-        
-        
-        
