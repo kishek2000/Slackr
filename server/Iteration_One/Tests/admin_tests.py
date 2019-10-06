b880a471030ee4eef32814201617b2dde9d798a6
Harry Wilson <z5264010@unsw.edu.au>
2019-10-06 13:21:47 +1100

    updated admin tests

--- server/Iteration_One/Tests/admin_tests.py
+++ server/Iteration_One/Tests/admin_tests.py
@@ -1,8 +1,8 @@
 import pytest
-@pytest.fixture
+
+@pytest.fixture():
 def register_account:
-    returned_dict = auth_register('example@gmail.com', '12345', 'dan', 'man')
-    return returned_dict
+    return auth_register('example@gmail.com', '12345', 'dan', 'man')
 # admin_userpermission_change tests
 # function inputs: token, u_id, permission_id
 # function returns:
@@ -11,31 +11,43 @@ Things to think about
     - What will permission_id be?
 '''
 
-def admin_no_return(register_account):
-    anr_token = returned_dict['token']
-    anr_u_id = returned_dict['u_id']
-    assert user_profile_setname(anr_token, anr_u_id, 'admin') == None
+#This test ensures that nothing is returned when calling the function
+def test_admin_no_return(register_account):
+    token = register_account['token']
+    u_id = register_account['u_id']
+    assert admin_userpermission_change(token, u_id, 'admin') == None
     
-def admin_no_input(register_account):
-    ani_token = returned_dict['token']
-    anr_u_id = returned_dict['u_id']
+#This function should raise an error when the permission_id is empty
+def test_admin_no_input(register_account):
+    token = register_account['token']
+    u_id = register_account['u_id']
     with pytest.raises(ValueError):
-        user_profile_setname(ani_token, ani_u_id, )
-        
-def admin_u_id_invalid(register_account):
-    auii_token = returned_dict['token']
-    anr_u_id = returned_dict['u_id']
-    with pytest.raises(ValueError):
-        user_profile_setname(auii_token, wrongu_id, 'admin')
-        
-def admin_permission_id_invalid(register_account):
-    apii_token = returned_dict['token']
-    anr_u_id = returned_dict['u_id']
-    with pytest.raises(ValueError):
-        user_profile_setname(auii_token, ani_u_id, wrongpermission)
+        admin_userpermission_change(token, u_id, '')
 
-def admin_user_unauthorised(register_account):
-    apii_token = returned_dict['token']
-    anr_u_id = returned_dict['u_id']
-    with pytest.raises(KeyError):
-        user_profile_setname(auii_token, ani_u_id, 'admin')
+#This function should raise an error when the u_id is invalid        
+def test_admin_u_id_invalid(register_account):
+    token = register_account['token']
+    u_id = register_account['u_id']
+    with pytest.raises(ValueError):
+        admin_userpermission_change(token, 'wrong u_id', 'admin')
+        
+#This function should raise an error when the token is invalid        
+def test_admin_u_id_invalid(register_account):
+    token = register_account['token']
+    u_id = register_account['u_id']
+    with pytest.raises(ValueError):
+        admin_userpermission_change('wrong token', u_id, 'admin')
+        
+#This function should raise an error when the permission_id is invalid        
+def test_admin_permission_id_invalid(register_account):
+    token = register_account['token']
+    u_id = register_account['u_id']
+    with pytest.raises(ValueError):
+        admin_userpermission_change(token, u_id, wrongpermission)
+
+#Test a user who is unauthorised to change 
+def test_admin_user_unauthorised(register_account):
+    token = register_account['token']
+    u_id = register_account['u_id']
+    with pytest.raises(AccessError):
+        admin_userpermission_change(token, u_id, 'admin')
