38592a7eadb60b7b731d96e875d96145dc85fd46
Shoan Desai <z5168024@ad.unsw.edu.au>
2019-10-16 19:22:20 +1100

    auth functions modified to match modified method of storing data

--- Server/functions/auth_functions.py
+++ Server/functions/auth_functions.py
@@ -10,26 +10,25 @@ def auth_login(email, password):
         raise ValueError("Invalid Email")        
    
     #Checking if email provided is registered
-    if email not in list_of_users:
+    if not email_registered(email):
         raise ValueError("Email Not Registered")           
     
     #Checking if password matches email
-    if list_of_users[email]["password"] != password:
+    if not email_matches_password(email, password):
         raise ValueError("Incorrect Password Entered")
     
     #Assign token to user if they are not logged in:
     
     for user in list_of_users:
         if user["email"] == email:
-            user["token"] = generate_token()
+            user["token"] = generate_token(email)
             return {user["u_id"], user["token"]}
     
 def auth_logout(token):
     
-    if token in list_of_valid_tokens:
-        list_of_valid_tokens.remove(token)
-    else:
-        print("Token already invalid")
+    for user in list_of_users:
+        if user["token"] == token:
+            user["token"] = None
     
 def auth_register(email, password, name_first, name_last):
 
@@ -44,31 +43,39 @@ def auth_register(email, password, name_first, name_last):
         raise ValueError("Invalid Password")
    
     #Check if for valid first and last names
-    if (len(name_first) < 1):
+    if (len(name_first) < 1 or len(name_first) > 50):
         raise ValueError("Invalid First Name")
         
-    if (len(name_last) < 1):
+    if (len(name_last) < 1 or len(name_last) > 50):
         raise ValueError("Invalid Last Name")
     
     print("VALIDDD")
     
     #Check if user is in list_of_users
     
-    if email in list_of_users:
+    if email_registered(email):
         raise ValueError("Email Provided Already in Use")
     
     #If not then add the user to list_of_users
     
-    list_of_users[email] = {"password": password, "u_id": generate_u_id(), "token" : generate_token(),
-                            "reset_code": None}
+    list_of_users.append({"email" : email, "password": password, "u_id": generate_u_id(),
+                          "token" : None, "reset_code": None , 
+                          "name_first": name_first, "name_last": name_last})
+                          
+    #Assign token
+    list_of_users[-1]["token"] = generate_token(email)
+                          
     print("USER ADDED")
  
-    return {list_of_users[email]["u_id"], list_of_users[email]["token"]}
+    return {list_of_users[-1]["u_id"], list_of_users[-1]["token"]}
     
 def auth_passwordreset_request(reset_email):
 
     reset_code = generate_reset_code()
-    list_of_users[reset_email]['reset_code'] = reset_code
+    
+    for user in list_of_users:
+        if user["email"] == reset_email:
+            user["reset_code"] = reset_code
     
     message = "Reset Code " + str(reset_code)
     
@@ -86,22 +93,23 @@ def auth_passwordreset_reset(reset_code, new_password):
     #Check if reset code belongs to any user. If not it is invalid
     
     for user in list_of_users: 
-        if list_of_users[user]['reset_code'] == None:
+        if user['reset_code'] == None:
             continue
     
-        if list_of_users[user]['reset_code'] == reset_code:
-            if valid_password(new_password):
-                list_of_users[user]['reset_code'] = None
-                list_of_users[user]['password'] = new_password
-                return
-            else:
-                raise ValueError("Invalid Password")
+        if user['reset_code'] == reset_code and valid_password(new_password):
+            
+            user['reset_code'] = None
+            user['password'] = new_password
+            return
+        else:
+            raise ValueError("Invalid Password")
             
     
     raise ValueError("Invalid Reset Code")
        
-#auth_register("shoandesai@gmail.com", "V@lidPassword123", "Shoan", "Desai")      
-#auth_register("santaIsRipped@gmail.com", "V@lidPassword123", "Santa", "Claus")    
-auth_login("santaIsRipped@gmail.com", "V@lidPassword123")    
-#auth_logout(12345) 
-#auth_passwordreset_request("shoandesai@gmail.com")
+auth_login("rajeshkumar@gmail.com", "V@lidPassword123") 
+auth_register("shoandesai@gmail.com", "V@lidPassword123", "Shoan", "Desai")      
+auth_register("santaIsRipped@gmail.com", "V@lidPassword123", "Santa", "Claus")    
+auth_login("santaIsRipped@gmail.com", "V@lidPassword123")   
+auth_logout(generate_token("santaIsRipped@gmail.com")) 
+auth_passwordreset_request("shoandesai@gmail.com")
