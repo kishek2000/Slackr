0ed74303600d79ed913bac02f848db06b95f97cc
Shoan Desai <z5168024@ad.unsw.edu.au>
2019-10-24 09:59:10 +1100

    standup tests passing

--- Server/functions/standup_functions.py
+++ Server/functions/standup_functions.py
@@ -2,6 +2,7 @@ from functions.Errors import AccessError
 from .helper_functions import *
 import datetime
 import time
+import threading
 
 def standup_start(token, channel_id):
     
@@ -23,15 +24,14 @@ def standup_start(token, channel_id):
       		
             #If no errors raised then start the startup
         elif channel_id == channel['channel_id'] and channel['standup_active'] == False:
+            
             channel['standup_active'] = True
 	
             #Wait for 15 minutes then end the startup
-
-            time.sleep(10)
-	
-            channel['standup_active'] = False
+            t = threading.Timer(60, end_standup_timer, [channel_id])
+            t.start()
     
-            return datetime.datetime.now()        		
+            return datetime.datetime.now() + datetime.timedelta(minutes = 1)     		
     
         		
 def standup_send(token, channel_id, message):
@@ -63,4 +63,10 @@ def standup_send(token, channel_id, message):
         if channel_id == channel['channel_id']:
             channel['standup_buffer'] = channel['standup_buffer'] + ": " + message
             
-  
+def end_standup_timer(channel_id):
+    
+    for channel in all_channels_messages:
+        if channel_id == channel['channel_id']:
+            channel['standup_active'] = False
+     
+            
