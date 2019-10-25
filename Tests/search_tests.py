2f706612de17be7210f2dca994c5fcb5b4fe6a57
Harry Wilson <z5264010@unsw.edu.au>
2019-10-25 15:51:48 +1100

    Search function now works

--- Tests/search_tests.py
+++ Tests/search_tests.py
@@ -1,7 +1,16 @@
 import pytest
+import sys
+sys.path.append('Server/')
+from functions.search_function import *
+from functions.auth_functions import auth_register
+from functions.helper_functions import *
+from functions.channel_functions import *
+from functions.message_functions import *
+
 @pytest.fixture
 def register_account():
-    returned_dict = auth_register('example@gmail.com', '12345', 'dan', 'man')
+    reset_data()
+    returned_dict = auth_register('example@gmail.com', 'Go0dPa>sword', 'dan', 'man')
     channel_id = channels_create(returned_dict['token'], "Channel A", False)
     return [returned_dict, channel_id]
 
@@ -13,8 +22,8 @@ def register_account():
 
 #The returned value from the function should be a dictionary
 def test_search_correct_return_type(register_account):
-    token = register_account['token']
-    assert type(search(token,'hello')) = type({})
+    token = register_account[0]['token']
+    assert type(search(token,'hello')) == type([])
 
 #The function should raise an error as there is no query_str    
 def test_search_no_input(register_account):
@@ -25,18 +34,18 @@ def test_search_no_input(register_account):
 #The function should return a dictionary with one message in it    
 def test_search_returned_value(register_account):
     token = register_account[0]['token']    
-    message_send(token, register_account[1]["channel_a"], "Hello")
+    message_send(token, register_account[1], "hello")
     returned_search = search(token, 'hello')
     assert len(returned_search) == 1
-    assert returned_search['message_1'] == 'hello'
+    assert returned_search[0]['message'] == 'hello'
     
 #This tests the function after sending then removing a message    
-def test_search_returned_value(register_account):
+def test_search_returned_value_after_remove(register_account):
     token = register_account[0]['token']    
-    message_send(token, register_account[1]["channel_a"], "Hello")
+    message_send(token, register_account[1], "hello")
     returned_search = search(token, 'hello')
     assert len(returned_search) == 1
-    assert returned_search['message_1'] == 'hello' 
-    message_remove(token, 0)
+    assert returned_search[0]['message'] == 'hello' 
+    message_remove(token, returned_search[0]['message_id'])
     returned_search = search(token, 'hello')
     assert len(returned_search) == 0
