5c7580c522e6154d7110bf403aa3acc8437b956e
Liam <z5254570@ad.unsw.edu.au>
2019-11-06 18:12:30 +1100

    I'm an idiot who only just figured out how to do reset_data properly

--- server/functions/data/reset_data.py
+++ server/functions/data/reset_data.py
@@ -1,10 +1,13 @@
 import pickle
-from ../helper_functions import *
+import sys
+sys.path.append("server/functions")
+from helper_functions import *
 reset_data()
 
-pickle.dump(all_channels_details, open('all_channels_details.p', 'wb'))
-pickle.dump(all_channels_messages, open('all_channels_messages.p', 'wb'))
-pickle.dump(all_channels_permissions, open('all_channels_permissions.p', 'wb'))
-pickle.dump(list_of_users, open('list_of_users.p', 'wb'))
-pickle.dump(number_of_channels, open('number_of_channels.p', 'wb'))
-pickle.dump(number_of_messages, open('number_of_messages.p', 'wb'))
+pickle.dump(all_channels_details, open('server/functions/data/all_channels_details.p', 'wb'))
+pickle.dump(all_channels_messages, open('server/functions/data/all_channels_messages.p', 'wb'))
+pickle.dump(all_channels_permissions, open('server/functions/data/all_channels_permissions.p', 'wb'))
+pickle.dump(list_of_users, open('server/functions/data/list_of_users.p', 'wb'))
+pickle.dump(number_of_channels, open('server/functions/data/number_of_channels.p', 'wb'))
+pickle.dump(number_of_messages, open('server/functions/data/number_of_messages.p', 'wb'))
+pickle.dump(number_of_users, open('server/functions/data/number_of_users.p', 'wb'))
