05a0add5c0ef0e69c8b8a1b38884abc66060d473
Harry Wilson <z5264010@unsw.edu.au>
2019-11-14 09:54:03 +1100

    User images works next to messages, need to iron out minor problems

--- server/functions/user_functions.py
+++ server/functions/user_functions.py
@@ -1,5 +1,6 @@
 """ user_functions """
 import sys
+#import os
 import urllib.request
 import requests
 from PIL import Image
@@ -25,7 +26,7 @@ def user_profile(token, u_id):
         raise ValueError("Invalid u_id")
     returned_dict = get_user_details(token)
 
-    user_dict = {'email': returned_dict['email'], 'name_first': returned_dict['name_first'], 'name_last': returned_dict['name_last'], 'handle_str': returned_dict['handle_str']}
+    user_dict = {'email': returned_dict['email'], 'name_first': returned_dict['name_first'], 'name_last': returned_dict['name_last'], 'handle_str': returned_dict['handle_str'], 'profile_img_url': returned_dict['profile_img_url']}
     return user_dict
 
 @authorise_token
@@ -91,10 +92,28 @@ def user_profiles_uploadphoto(token, img_url, x_start, y_start, x_end, y_end, ur
     if returned_status.status_code != 200:
         raise ValueError("Invalid img_url")
 
-    #image_path = '/server/functions/data/user_images/' + token + '.png'
-    #image_dir = '.' + image_path
+    
     image_path = token + '.jpg'
+    #x = datetime.datetime.now()
+    #################################
+    #return_dict = get_user_details(token)
+    
+    #image_path = return_dict['u_id'] + x + '.jpg'
+    ###############################
     image_dir = '.' + '/static/' + image_path
+    
+    
+    '''
+    return_dict = get_user_details(token)
+    if return_dict['profile_img_url'] is not None:
+        print('yo')
+        os.remove(image_dir)
+        for user in list_of_users:
+            if user['token'] == token:
+                user['profile_img_url'] = None
+    '''
+    
+    
     urllib.request.urlretrieve(str(img_url), image_dir)
     #image = Image.open(requests.get(str(img_url), stream=True).raw)
     image = Image.open(image_dir)
